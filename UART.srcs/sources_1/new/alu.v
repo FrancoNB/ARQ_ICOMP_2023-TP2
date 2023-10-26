5fc778efa972583f9c3a1aa03efd040903f2038f
FrancoNB <francobottini99@mi.unc.edu.ar>
2023-10-26 15:53:08 -0300

    Agrego la ALU del TP 1

---
+++ UART.srcs/sources_1/new/alu.v
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Facultad de Ciencias Exactas Físicas y Naturales
+// Authors: Robledo Valentin y Bottini Franco Nicolas 
+// 
+// Module Name: ALU
+// Project Name: TP 2 - Arquitectura de Computadoras 2023 - UART
+// Description: Implementación de una unidad aritmetico logica con las operaciones ADD, SUB, AND, OR, XOR, SRA, SRL, NOR, RESET
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+
+`include "alu.vh"
+
+module alu
+    #(
+        parameter IO_BUS_WIDTH = `IO_BUS_WIDTH
+    )
+    (
+        input  wire [`OP_CODE_WIDTH - 1 : 0] op_code, 
+        input  wire [IO_BUS_WIDTH - 1 : 0]   data_a,
+        input  wire [IO_BUS_WIDTH - 1 : 0]   data_b,
+        
+        output wire [IO_BUS_WIDTH - 1 : 0]   out_data
+    );
+    
+    reg[IO_BUS_WIDTH - 1 : 0] result;
+    
+    always@(*)
+    begin
+        case(op_code)
+            `OP_ADD  : result <= data_a + data_b;
+            `OP_SUB  : result <= data_a - data_b;
+            `OP_AND  : result <= data_a & data_b;
+            `OP_OR   : result <= data_a | data_b;
+            `OP_XOR  : result <= data_a ^ data_b;
+            `OP_SRA  : result <= data_a >> data_b;
+            `OP_SRL  : result <= data_a >>> data_b;
+            `OP_NOR  : result <= ~(data_a | data_b);
+            default  : result <= {IO_BUS_WIDTH {1'bz}};
+        endcase
+    end
+    
+    assign out_data = result; 
+    
+endmodule
